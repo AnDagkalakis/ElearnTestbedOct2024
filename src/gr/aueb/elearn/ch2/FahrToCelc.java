f0d762db89b2509b2d65277fadd27ae5a7afed74
AnDagkalakis <andagalakis@gmail.com>
2024-11-03 18:44:37 +0200

    Reforms the numbers the users enters for Day,Month,Year into a Date. v0.1

---
+++ src/gr/aueb/elearn/ch2/FahrToCelc.java
@@ -0,0 +1,28 @@
+package gr.aueb.elearn.ch2;
+/**
+ * Converts Fahrenheit temperature into Celcius
+ * in an Integer number.
+ *
+ * @author AnDagkalakis
+ * @version 0.1
+ */
+
+import java.util.Scanner;
+
+public class FahrToCelc {
+
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        int fahr;
+        int celc;
+
+
+        System.out.println("Please input the temperature in Fahrenheit");
+
+        fahr= in.nextInt();
+        celc= 5 *  (fahr - 32) / 9;
+
+        System.out.printf("The temperature in Celcius is %d", celc);
+
+    }
+}
